8d91cb8655766c7d462075816bf0bfd2046723c1
alezlimon <ale18pm@gmail.com>
2025-12-15 16:56:36 +0100

    docs: apuntes detallados y refactor completo del lab

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,9 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      {tweetsArray.map((tweet, idx) => (
+        <Tweet key={idx} tweet={tweet} />
+      ))}
     </div>
   );
 }
